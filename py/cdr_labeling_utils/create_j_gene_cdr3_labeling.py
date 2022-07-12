7a4b7cd596e50936d9ee7dc1dfbc74b8f131da4a
Yana Safonova <safonova.yana@gmail.com>
2022-07-12 13:31:39 -0700

    fixes

--- py/cdr_labeling_utils/create_j_gene_cdr3_labeling.py
+++ py/cdr_labeling_utils/create_j_gene_cdr3_labeling.py
@@ -4,6 +4,9 @@ from Bio import SeqIO
 
 gene_fasta = sys.argv[1]
 output_fname = sys.argv[2]
+cons_seq = 'tggggc'
+if len(sys.argv) == 4:
+    cons_seq = sys.argv[3]
 
 aligned_genes = []
 for r in SeqIO.parse(gene_fasta, 'fasta'):
@@ -11,7 +14,6 @@ for r in SeqIO.parse(gene_fasta, 'fasta'):
     aligned_genes.append(r)
 
 output_fh = open(output_fname, 'w')
-cons_seq = 'tggggc'
 for gene in aligned_genes:
     w_pos = gene.seq.lower().find(cons_seq)
     output_fh.write(gene.id + '\t' + str(w_pos) + '\t' + str(w_pos + 1) + '\n')
